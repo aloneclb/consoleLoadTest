2c44bfa0f7ba1a0f27371188ae5d1c278c8be9e3
tunahan <tunahancelebi@derinbilgi.com.tr>
2023-06-13 13:14:54 +0300

    Aciklamalar düzenlendi.

--- multiThreadLoadTest.py
+++ multiThreadLoadTest.py
@@ -3,14 +3,12 @@ import aiohttp
 import time
 import concurrent.futures
 
-# Yükleme testi için gereken parametreler
-url = "https://denemekoleji.okulsepeti.com.tr/sinif-paket-detayi/59655/"  # İstek göndermek istediğiniz URL'yi buraya yerleştirin
-total_requests = 30
-# 10 saniye cinsinden süre
+# parametreler
+url = "https://denemekoleji.panelkitap.com/sinif-paket-detayi/59655/" 
+total_requests = 300
 duration = 10
 
-# İstek başına geçen süreleri saklamak için boş bir liste oluşturun
-response_times = []
+gecen_sureler = [] # gecen süreler
 
 # Session bilgisi için headers ayarlayın
 headers = {"Cookie": "session=sessionid=hijmo50ud2m6y4h7vtkt83a5blz81rqx;"}
@@ -28,21 +26,20 @@ async def load_test():
 
 # Asenkron istek gönderme işlevi
 async def send_request(session):
-    start_time = time.time()  # İstek gönderme zamanını kaydedin
+    start_time = time.time()  # İstek gönderme zamanını al
 
     async with session.get(url) as response:
         response_time = time.time() - start_time  # İstek yanıtının alındığı süreyi hesaplayın
-        response_times.append((start_time, response_time))  # İstek süresini listeye ekleyin
+        gecen_sureler.append((start_time, response_time))  # İstek süresini listeye ekleyin
 
 # Asenkron yükleme testini başlatın
 async def run_load_test():
     loop = asyncio.get_running_loop()
-    start_time = time.time()  # Yükleme testinin başlangıç zamanını kaydedin
-    end_time = start_time + duration  # Yükleme testinin bitiş zamanını hesaplayın
+    start_time = time.time()  # Yükleme testinin başlangıç zamanını al
+    end_time = start_time + duration  # Yükleme testinin bitiş zamanını al
     while time.time() < end_time:
         await load_test()
 
-# Yükleme testini aynı anda 10 thread'de gerçekleştirmek için yardımcı işlev
 def run_in_thread():
     asyncio.run(run_load_test())
 
@@ -52,7 +49,7 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         executor.submit(run_in_thread)
 
 # # Elde edilen istek sürelerini yazdırın
-for i, response_time in enumerate(response_times, start=1):
+for i, response_time in enumerate(gecen_sureler, start=1):
     request_number = i
     elapsed_time = response_time[1]
     print(f"Istek {request_number}: {elapsed_time:.2f} saniye")
